f8344103fec6d501075baae1dcb1442d5aa6dee1
Scott Kingsley <scott.kingsley@kingsleyimages.comgh auth logingit config --global user.email scott.kingsley@kingsleyimages.co>
2024-12-30 15:07:34 -0700

    responsive css

--- js/javascript.js
+++ js/javascript.js
@@ -4,5 +4,5 @@ const submitButton = document.querySelector('#submit');
 form.addEventListener('submit', (e) => {
   submitButton.disabled = true;
   e.preventDefault();
-  window.location.href = window.location.origin + '/html/success.html';
+  window.location.href = window.location.origin + '/success.html';
 });
